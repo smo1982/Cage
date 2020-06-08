3b9c476aa1b7584a009e2b8af656757fe261b1ea
StefanMoosmann <smo1982@gmx.at>
2020-06-08 14:45:09 +0200

    first init

---
+++ Cage/Program.cs
@@ -0,0 +1,25 @@
+﻿using Cage.models;
+using System;
+
+namespace Cage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Monkey monkey = new Monkey("Aff");
+            Tiger tiger = new Tiger("katz");
+            Cage<Monkey> monkeyCage = new Cage<Monkey>();
+            monkeyCage.AddAnimal(monkey);
+            Cage<Tiger> tigerCage = new Cage<Tiger>();
+            tigerCage.AddAnimal(tiger);
+            monkeyCage.PrintAnimal();
+            tigerCage.PrintAnimal();
+            monkeyCage.RemoveAnimal(monkey);
+            monkeyCage.PrintAnimal();
+            tigerCage.PrintAnimal();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cage/models/Cage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cage.models
+{
+    class Cage<T>
+    {
+        private List<T> list = new List<T>();
+
+        public void AddAnimal(T animal)
+        {
+            list.Add(animal);
+        }
+
+        public void RemoveAnimal(T animal)
+        {
+            list.Remove(animal);
+        }
+
+        public void PrintAnimal()
+        {
+            foreach(T objekt in list)
+            {
+                Console.WriteLine(objekt);
+            }
+        }
+    }
+}
